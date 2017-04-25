8e67f9ba0f48df1b0c4311c5032b346ad0ca7fec
Grady Shelton and Tyler Stephenson <tyler@>
2017-04-25 08:57:32 -0700

    pass tests adding date and note methods to places

--- lib/place.rb
+++ lib/place.rb
@@ -1,23 +1,34 @@
 class Place
 
   @@all_places = []
-  define_method(:initialize) do |location|
+
+  define_method(:initialize) do |location, date, notes|
     @location = location
+    @date = date
+    @notes = notes
   end
 
-  define_method(:location) do
-    @location
+  define_singleton_method(:clear) do
+    @@all_places = []
   end
 
   define_singleton_method(:all) do
     @@all_places
   end
 
+  define_method(:location) do
+    @location
+  end
+
   define_method(:save) do
     @@all_places.push(self)
   end
 
-  define_singleton_method(:clear) do
-    @@all_places = []
+  define_method(:date) do
+    @date
+  end
+
+  define_method(:notes) do
+    @notes
   end
 end
--- spec/place_spec.rb
+++ spec/place_spec.rb
@@ -2,12 +2,17 @@ require('rspec')
 require('place')
 
 describe(Place) do
+  before() do
+    Place.clear()
+  end
+
   describe("#location") do
     it('lets you give place a location') do
-      test_place = Place.new("New York")
+      test_place = Place.new("New York", '1884', 'hot as hell')
       expect(test_place.location()).to(include("New York"))
     end
   end
+
 
   describe(".all") do
     it('is empty at first') do
@@ -17,7 +22,7 @@ describe(Place) do
 
   describe("#save") do
     it('adds a place to the array of saved places') do
-      test_place = Place.new("New York")
+      test_place = Place.new("New York", '1884', 'hot as hell')
       test_place.save()
       expect(Place.all()).to(eq([test_place]))
     end
@@ -25,9 +30,23 @@ describe(Place) do
 
   describe(".clear") do
     it('empties out all saved places') do
-      Place.new("New York").save()
+      Place.new("New York", '1884', 'hot as hell').save()
       Place.clear()
       expect(Place.all()).to(eq([]))
+    end
+  end
+
+  describe('#date') do
+    it('lets you give a place a date for when you visited') do
+      test_place = Place.new("New York", '1884', 'hot as hell')
+      expect(test_place.date()).to(eq('1884'))
+    end
+  end
+
+  describe('#notes') do
+    it('lets you add notes for a place') do
+      test_place = Place.new("New York", '1884', 'hot as hell')
+      expect(test_place.notes()).to(eq('hot as hell'))
     end
   end
 end
