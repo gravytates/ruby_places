e257ede6bf624ef1060b7b12c3687ff81173b608
Grady Shelton and Tyler Stephenson <grady@>
2017-04-25 08:25:46 -0700

    pass second test showing empty array for class

--- lib/place.rb
+++ lib/place.rb
@@ -6,4 +6,8 @@ class Place
   define_method(:location) do
     @location
   end
+
+  define_singleton_method(:all) do
+    []
+  end
 end
--- spec/place_spec.rb
+++ spec/place_spec.rb
@@ -8,4 +8,10 @@ describe(Place) do
       expect(test_place.location()).to(include("New York"))
     end
   end
+
+  describe(".all") do
+    it('is empty at first') do
+      expect(Place.all()).to(eq([]))
+    end
+  end
 end
